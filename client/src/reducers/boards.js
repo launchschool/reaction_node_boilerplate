f80bae22df4326cb172e0b7c5e8e9abe5c423562
Ian Eustis <moose@Caramel-Corn.local>
2021-03-24 12:03:13 -0700

    day2

--- client/src/reducers/boards.js
+++ client/src/reducers/boards.js
@@ -7,7 +7,17 @@ export default function boards(state = [], action) {
       const newBoard = action.board;
       return state.concat(newBoard);
     }
+    case "FETCH_BOARD_SUCCESS": {
+      // remove list from board
+    }
     default:
       return state;
   }
 }
+
+// Need list actions and card actions
+// list actions will remove cards from list on Fetch Success
+
+// always call cards reducer, list reducer, board reducer
+// dispatch as few actions as possible
+// but all reducers will listent
